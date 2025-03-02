b33b49cf72906c8497306bdc709a70a89bed2663
Piyush Dubey <dpiyush28@gmail.com>
2025-03-02 11:06:08 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RealityRishi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
